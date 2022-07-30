974bfec507ef75ce900b225f1f1ee6400f54215a
Степан Боднарь <stepan.bodnar43@gmail.com>
2022-07-30 22:22:45 +0300

    Добавил SplashScreen

--- MusicPlayer/MusicPlayer.Android/MainActivity.cs
+++ MusicPlayer/MusicPlayer.Android/MainActivity.cs
@@ -7,12 +7,13 @@ using Android.OS;
 
 namespace MusicPlayer.Droid
 {
-    [Activity(Label = "GuitarApp", Icon = "@drawable/logo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "GuitarApp", Icon = "@drawable/logo", Theme = "@style/Theme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            SetTheme(Resource.Style.MainTheme);
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
